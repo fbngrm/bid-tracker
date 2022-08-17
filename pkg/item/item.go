a153562176518b1a77d5b08428448a02a99b33ad
fabian <fbngrm@gmail.com>
2022-08-17 17:41:31 +0200

    chore: return errors for addBid

--- pkg/item/item.go
+++ pkg/item/item.go
@@ -1,13 +1,17 @@
 package item
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/fbngrm/bid-tracker/pkg/bid"
 	"github.com/google/uuid"
 )
 
-// Note, Item is not safe for concurrent use.
+// Note, Item is not safe for concurrent use and should be used via the
+// store only. We encapsulate fields and methods that need to be protected
+// on package boundary.
+// TODO: provide serialziation for global scope
 type Item struct {
 	ID      uuid.UUID // uuid RFC 4122
 	name    string
@@ -18,6 +22,7 @@ type Item struct {
 func NewItem(name string) (*Item, error) {
 	id, err := uuid.NewRandom()
 	if err != nil {
+		// note, we must escape name since it is potentially harmful user input that we don't want to log unescaped
 		return nil, fmt.Errorf("could not generate id [%q]: %w", name, err)
 	}
 	return &Item{
@@ -27,12 +32,12 @@ func NewItem(name string) (*Item, error) {
 	}, nil
 }
 
-func (i *Item) Bid(b *bid.Bid) {
+func (i *Item) addBid(b *bid.Bid) error {
 	if b == nil {
-		return
+		return errors.New("could not add bid, bid is nil")
 	}
 	if i == nil {
-		return
+		return fmt.Errorf("could not add bid [%s], item is nil", b.ID.String())
 	}
 
 	// first bid is always the highest
@@ -43,12 +48,13 @@ func (i *Item) Bid(b *bid.Bid) {
 	// add the bid
 	// likely, performance could be optimized by keeping track of
 	// length and capacity and utilizing the underlaying array manually
-	// this is error prone when not done properly so I consider it out
-	// of scope of this challenge
+	// though, this is error prone when not done properly so I consider
+	// it out of scope of this challenge
 	i.bids = append(i.bids, b)
 
 	// earlier bids with the same amount are staying highest
 	if b.Amount > i.highest.Amount {
 		i.highest = b
 	}
+	return nil
 }
